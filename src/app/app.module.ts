39a5b3e2119df8f2490bf2018446fce27710e1ad
Pavan Komandur <pavankomandur@Pavans-MacBook-Pro.local>
2020-05-27 21:56:24 -0400

    initial checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {PhoneSuggestService} from './app.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [PhoneSuggestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
